ced2dd2244a0d9a14aaf20058809771c1af4f59d
samet Tunca <tuncasamet00@gmail.com>
2026-05-28 15:21:39 +0300

    add terraform and k8s manifests

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "voting" {
+  name     = "voting-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.voting.location 
+  resource_group_name = azurerm_resource_group.voting.name 
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
